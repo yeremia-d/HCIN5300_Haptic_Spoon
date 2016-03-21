f5a176ad7d8de08f5913ad85ecfd1f618b5bf425
yeremia-d <yeremia-d@users.noreply.github.com>
2016-03-21 16:57:04 -0400

    Updated Arduino Firmware

--- haptic_spoon.ino
+++ haptic_spoon.ino
@@ -1,92 +1,101 @@
-// Include Wire Library
 #include <Wire.h>
+#include "Adafruit_DRV2605.h"
 
-// Include Adafruit haptic driver library
-#include <Adafruit_DRV2605.h>
-
-// Instance of Haptic Driver
-Adafruit_DRV2605 haptic_driver;
-
-// Haptic Spoon State
-int spoon_state;
-
-// Settings
-int inter_pattern_delay = 1000;
+Adafruit_DRV2605 drv;
+int mode;
+uint8_t beat_intensity, beat_0;
+int beat_delay, beat_length;
 
 void setup() {
-  // Initiate Serial Connection
   Serial.begin(9600);
-  
-  // Send state "Serial Connection Established"
-  Serial.println('1'); 
+  drv.begin();
+  drv.useLRA();
+  drv.setMode(DRV2605_MODE_REALTIME);
 
-  // Initiate Haptic Driver
-  haptic_driver.begin();
-  haptic_driver.useLRA();
-  // Set Haptic Driver to Realtime mode
-  haptic_driver.setMode(DRV2605_MODE_INTTRIG);
-
-  // Send State "Haptic Driver Ready"
-  Serial.println('2');
-
-  // Initialize Spoon State
-  spoon_state = 0;
+  mode = 0;
+  beat_0 = 0x00;
+  beat_length = 500;
 
 }
 
-
 void loop() {
+ 
+  // Read serial buffer
+  if(Serial.available() !=0) {
+    mode = Serial.read() - '0';
 
-  // If there is data in the serial buffer, read and match
-  if(Serial.available() != 0) {
-
-    // Read-in and set Spoon State
-    spoon_state = Serial.read() - '0'; 
-  }
-  Serial.println(spoon_state);
-  
-  // Select Pattern based on spoon state
-  switch(spoon_state) {
-    case 1: // Hot Pattern 1
-    haptic_driver.setWaveform(0, 84);
-    haptic_driver.setWaveform(1, 58);
-    haptic_driver.setWaveform(2, 95);
-    inter_pattern_delay = 10;
-    break;
-
-    case 2: // Hot Pattern 2
-    haptic_driver.setWaveform(0, 2);
-    break;
-
-    case 3: // Warm Pattern 1
-    haptic_driver.setWaveform(0, 11);
-    inter_pattern_delay = 4000;
-    break;
-
-    case 4: // Warm Pattern 2
-    haptic_driver.setWaveform(0, 40);
-    inter_pattern_delay = 4000;
-    break;
-
-    case 5: // Cold Pattern 1
-    haptic_driver.setWaveform(0, 56);
-    haptic_driver.setWaveform(1, 27);
-    haptic_driver.setWaveform(2, 56);
-    haptic_driver.setWaveform(3, 27);
-    inter_pattern_delay = 0;
-    break;
-
-    case 6: // Cold Pattern 2
-    haptic_driver.setWaveform(0, 6);
-    break;
-
-    // Default spoon state
-    default: haptic_driver.setWaveform(0, 0);
-    break;
+    // set appropriate mode parameters
+    setMode(mode);
   }
   
-  haptic_driver.go();
-  delay(inter_pattern_delay);
-  
+  // play pattern
+  playPattern();
 
+}
+
+void playPattern()
+{
+  drv.setRealtimeValue(beat_intensity);
+  delay(beat_length);
+  drv.setRealtimeValue(beat_0);
+  delay(beat_delay);
+}
+
+void setMode(int mode) {
+  switch(mode) {
+    case 0:
+      beat_intensity = 0x00;
+      beat_delay     = 100;
+    break;
+
+    case 1:
+      beat_intensity = 0x44;
+      beat_delay     = 1500;
+    break;
+
+    case 2:
+      beat_intensity = 0x44;
+      beat_delay     = 1000;
+    break;
+
+    case 3:
+      beat_intensity = 0x44;
+      beat_delay     = 300;
+    break;
+
+    case 4:
+      beat_intensity = 0x60;
+      beat_delay     = 1500;
+    break;
+
+    case 5:
+      beat_intensity = 0x60;
+      beat_delay     = 1000;
+    break;
+
+    case 6:
+      beat_intensity = 0x60;
+      beat_delay     = 300;
+    break;
+
+    case 7:
+      beat_intensity = 0x79;
+      beat_delay     = 1500;
+    break;
+
+    case 8:
+      beat_intensity = 0x79;
+      beat_delay     = 1000;
+    break;
+
+    case 9:
+      beat_intensity = 0x79;
+      beat_delay     = 300;
+    break;
+
+    default:
+      beat_intensity = 0x00;
+      beat_delay     = 100;
+    break;
+  }
 }
